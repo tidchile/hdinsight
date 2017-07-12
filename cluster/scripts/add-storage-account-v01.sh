1fac5195859d6d588b67fced23f23556fca7aa79
bponte <bponte@gmail.com>
2017-07-12 14:43:25 -0400

    Update add-storage-account-v01.sh
    add safe key encrypted, stop start HIVE service, wait

--- cluster/scripts/add-storage-account-v01.sh
+++ cluster/scripts/add-storage-account-v01.sh
@@ -7,23 +7,40 @@ ACTIVEAMBARIHOST=headnodehost
 usage() {
   echo ""
   echo "Usage: sudo -E bash add-storage-account-v01.sh";
+  echo "If -p option is specified, then storage account key will be stored in plain text. Otherwise, it will be encrypted."
   echo "This script does NOT require Ambari username and password";
   exit 132;
 }
-        
+wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
+
 checkHostNameAndSetClusterName() {
-  fullHostName=$(hostname -f)
-  echo "fullHostName=$fullHostName"
-  CLUSTERNAME=$(sed -n -e 's/.*\.\(.*\)-ssh.*/\1/p' <<< $fullHostName)
-  if [ -z "$CLUSTERNAME" ]; then
-    CLUSTERNAME=$(echo -e "import hdinsight_common.ClusterManifestParser as ClusterManifestParser\nprint ClusterManifestParser.parse_local_manifest().deployment.cluster_name" | python)
-    if [ $? -ne 0 ]; then
-      echo "[ERROR] Cannot determine cluster name. Exiting!"
-      exit 133
-    fi
+  PRIMARYHEADNODE=`get_primary_headnode`
+    
+  #Check if values retrieved are empty, if yes, exit with error
+  if [[ -z $PRIMARYHEADNODE ]]; then
+  echo "Could not determine primary headnode."
+  exit 139
   fi
-  echo "Cluster Name=$CLUSTERNAME"
+
+  fullHostName=$(hostname -f)
+    echo "fullHostName=$fullHostName. Lower case: ${fullHostName,,}"
+    echo "primary headnode=$PRIMARYHEADNODE. Lower case: ${PRIMARYHEADNODE,,}"
+    if [ "${fullHostName,,}" != "${PRIMARYHEADNODE,,}" ]; then
+        echo "$fullHostName is not primary headnode. This script has to be run on $PRIMARYHEADNODE."
+        exit 0
+    fi
+    CLUSTERNAME=$(sed -n -e 's/.*\.\(.*\)-ssh.*/\1/p' <<< $fullHostName)
+    if [ -z "$CLUSTERNAME" ]; then
+        CLUSTERNAME=$(echo -e "import hdinsight_common.ClusterManifestParser as ClusterManifestParser\nprint ClusterManifestParser.parse_local_manifest().deployment.cluster_name" | python)
+        if [ $? -ne 0 ]; then
+            echo "[ERROR] Cannot determine cluster name. Exiting!"
+            exit 133
+        fi
+    fi
+    echo "Cluster Name=$CLUSTERNAME"
 }
+
+checkHostNameAndSetClusterName
 
 validateUsernameAndPassword() {
   coreSiteContent=$(bash $AMBARICONFIGS_SH -u $USERID -p $PASSWD get $ACTIVEAMBARIHOST $CLUSTERNAME core-site)
@@ -34,13 +51,60 @@ validateUsernameAndPassword() {
 }
 
 updateAmbariConfigs() {
-  updateResult=$(bash $AMBARICONFIGS_SH -u $USERID -p $PASSWD set $ACTIVEAMBARIHOST $CLUSTERNAME core-site $1 $2)
-  if [[ $updateResult != *"Tag:version"* ]] && [[ $updateResult == *"[ERROR]"* ]]; then
-    echo "[ERROR] Failed to update core-site. Exiting!"
-    echo $updateResult
-    exit 135
-  fi
-  echo "Added $1 = $2"
+  STORAGEACCOUNTNAME=$1
+  if [ "$DISABLEENCRYPTION" == true ]; then
+    echo "Encryption is disabled. No changes will be made to storage account key."
+  else
+    echo "Encrypting storage account key"
+    echo "Getting encryption cert"
+    for cert in `sudo ls /var/lib/waagent/*.crt`
+    do
+      SUBJECT=`sudo openssl x509 -in $cert -noout -subject`
+      if [[ $SUBJECT == *"cluster-$CLUSTERNAME-"* ]]; then
+          CERT=$cert
+          break
+      fi
+    done
+
+    if [ -z "$CERT" ];then
+      echo "Could not locate cert for encryption"
+      exit 142
+    fi
+
+    echo $2 | sudo openssl cms -encrypt -outform PEM -out storagekey.txt $CERT
+    if (( $? )); then
+      echo "Could not encrypt storage account key"
+      exit 140
+    fi
+
+    STORAGEACCOUNTKEY=$(echo -e "import re\n\nfile = open('storagekey.txt', 'r')\nfor line in file.read().splitlines():\n\tif '-----BEGIN CMS-----' in line or '-----END CMS-----' in line:\n\t\tcontinue\n\telse:\n\t\tprint line\nfile.close()" | sudo python)
+    STORAGEACCOUNTKEY=$(echo $STORAGEACCOUNTKEY | tr -d ' ')
+    if [ -z "$STORAGEACCOUNTKEY" ];
+    then
+      echo "Storage account key could not be stripped off header values form encrypted key"
+      exit 141
+    fi
+    rm storagekey.txt
+  fi 
+
+
+  updateResult=$(bash $AMBARICONFIGS_SH -u $USERID -p $PASSWD set $ACTIVEAMBARIHOST $CLUSTERNAME core-site "fs.azure.account.key.$STORAGEACCOUNTNAME.blob.core.windows.net" "$STORAGEACCOUNTKEY")
+    
+    if [[ $updateResult != *"Tag:version"* ]] && [[ $updateResult == *"[ERROR]"* ]]; then
+        echo "[ERROR] Failed to update core-site. Exiting!"
+        echo $updateResult
+        exit 135
+    fi
+    echo "Added property: 'fs.azure.account.key.$STORAGEACCOUNTNAME.blob.core.windows.net' with storage account key"
+
+	updateResult=$(bash $AMBARICONFIGS_SH -u $USERID -p $PASSWD set $ACTIVEAMBARIHOST $CLUSTERNAME core-site "fs.azure.account.keyprovider.$STORAGEACCOUNTNAME.blob.core.windows.net" "org.apache.hadoop.fs.azure.$KEYPROVIDER")    
+	if [[ $updateResult != *"Tag:version"* ]] && [[ $updateResult == *"[ERROR]"* ]]; then
+		echo "[ERROR] Failed to update core-site. Exiting!"
+		echo $updateResult
+		exit 135
+	fi
+	echo "Added property: 'fs.azure.account.keyprovider.$STORAGEACCOUNTNAME.blob.core.windows.net':org.apache.hadoop.fs.azure.$KEYPROVIDER "
+
 }
 
 stopServiceViaRest() {
@@ -91,28 +155,51 @@ if [ "$(id -u)" != "0" ]; then
   echo "[ERROR] The script has to be run as root."
   usage
 fi
+
 USERID=$(echo -e "import hdinsight_common.Constants as Constants\nprint Constants.AMBARI_WATCHDOG_USERNAME" | python)
+
 echo "USERID=$USERID"
+
 PASSWD=$(echo -e "import hdinsight_common.ClusterManifestParser as ClusterManifestParser\nimport hdinsight_common.Constants as Constants\nimport base64\nbase64pwd = ClusterManifestParser.parse_local_manifest().ambari_users.usersmap[Constants.AMBARI_WATCHDOG_USERNAME].password\nprint base64.b64decode(base64pwd)" | python)
 
-checkHostNameAndSetClusterName
 validateUsernameAndPassword
 
-args=("$@")  
-for ((i=0; i<${#args[@]}; i+=2)); do
+args=("$@")
+ARG_LEN=${#args[@]}
+if [ "${args[$ARG_LEN-1]}" == "-p" ]
+  then
+    DISABLEENCRYPTION=true
+    KEYPROVIDER=SimpleKeyProvider
+    ARG_LEN=$(( ARG_LEN - 1 ))
+    echo "Key encryption is disabled."
+  else
+    DISABLEENCRYPTION=false
+    KEYPROVIDER=ShellDecryptionKeyProvider
+    echo "Key encryption is enabled"
+fi
+
+
+for ((i=0; i<$ARG_LEN; i+=2)); do
   if [ -z "${args[i]}" ] || [ -z "${args[i+1]}" ] || checkAccountName ${args[i]} || keyIsBase64Encoded ${args[i+1]}; then
     echo "[ERROR] ${args[i]} ${args[i+1]} "
-  else    
-    updateAmbariConfigs fs.azure.account.key.${args[i]}.blob.core.windows.net ${args[i+1]}
+  else
+    updateAmbariConfigs ${args[i]} ${args[i+1]}
   fi
 done
 
-stopServiceViaRest HDFS
-stopServiceViaRest YARN
-stopServiceViaRest MAPREDUCE2
-stopServiceViaRest OOZIE
+if (( $ARG_LEN>1 )); then
+  stopServiceViaRest OOZIE
+  stopServiceViaRest YARN
+  stopServiceViaRest MAPREDUCE2
+  stopServiceViaRest HDFS
+  stopServiceViaRest HIVE
 
-startServiceViaRest YARN
-startServiceViaRest MAPREDUCE2
-startServiceViaRest OOZIE
-startServiceViaRest HDFS
+  #sleep for 30 seconds to reduce the possibility of race condition in stopping and starting services
+  sleep 30
+
+  startServiceViaRest HIVE
+  startServiceViaRest HDFS
+  startServiceViaRest MAPREDUCE2
+  startServiceViaRest YARN
+  startServiceViaRest OOZIE
+fi
